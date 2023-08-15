b939961fda2e94d012a94b14f333b0a24bd350fc
PeterGeorgiapg <pacers0406@gmail.com>
2023-08-15 18:47:16 +1000

    complete graph building, add a target png

--- main/graph_generation.py
+++ main/graph_generation.py
@@ -10,15 +10,20 @@ from dgl.data.utils import save_graphs
 import h5py
 import yaml
 
+
+import urllib.request
+from urllib.request import urlopen
+import ssl
+import json
+
 # Read the YAML configuration file
 with open("config/base.yaml", 'r') as config_file:
     config_data = yaml.safe_load(config_file)
 
-# Retrieve the value for the key "Place"
-place_value = config_data.get("histocartography_path")
-print(place_value)
-
+# # Retrieve the value for the key "Place"
+# place_value = config_data.get("histocartography_path")
 sys.path.append('../histocartography/histocartography')
+sys.path.append('../histocartography')
 from preprocessing import (
     VahadaneStainNormalizer,         # stain normalizer
     NucleiExtractor,                 # nuclei detector 
@@ -31,14 +36,17 @@ from preprocessing import (
 )
 
 class GraphBuilding:
-    def __init__(self,image_target_path):
-        self.image_path = image_target_path
-        self.stain_normalizer = VahadaneStainNormalizer(target_path=self.image_path)
+    def __init__(self,target_image_path):
+        self.target_image_path = target_image_path
+        self.stain_normalizer = VahadaneStainNormalizer(target_path=self.target_image_path)
 
         self.assignment_mat_builder = AssignmnentMatrixBuilder()
 
         # Store Image name that might have failed
         self.image_failed = []
+
+        self.cell_graph_builder()
+        self.tissue_graph_builder()
     
     def cell_graph_builder(self):
         self.nuclei_detector = NucleiExtractor()
@@ -57,7 +65,7 @@ class GraphBuilding:
         # normalized centroid features.
         self.knn_graph_builder = KNNGraphBuilder(k=5, thresh=50, add_loc_feats=True)
     
-    def build_tissue_graph_builder(self):
+    def tissue_graph_builder(self):
         # a define nuclei extractor    
         self.tissue_detector = ColorMergedSuperpixelExtractor(
             superpixel_size=500,
@@ -99,49 +107,58 @@ class GraphBuilding:
         #   Get a list of image and store them
         folder = os.path.join(images_folder, split)
         images_path = [file for file in os.listdir(folder) if file.endswith('.png')]
-
+        print(images_path)
         for image_path in tqdm(images_path):
             _, image_name = os.path.split(image_path)
-            image = np.array(Image.open(image_path))
+            print(image_path)
+            read_path = os.path.join(folder,image_path)
+            image = np.array(Image.open(read_path))
+            print(f"Image has shape {image.shape}")
         #   Get the store path
             cg_out = os.path.join(store_path, 'cell_graphs', split, image_name.replace('.png', '.bin'))
             tg_out = os.path.join(store_path, 'tissue_graphs', split, image_name.replace('.png', '.bin'))
-            assign_out = os.path.join(store_path, 'assignment_matrices', split, image_name.replace('.png', '.h5'))
+            assign_out = os.path.join(store_path, 'assignment_mat', split, image_name.replace('.png', '.h5'))
 
-        #   Stain Normalisation
-        try: 
-            image = self.normalizer.process(image)
-        except:
-            print('Warning: {} failed during stain normalization.'.format(image_path))
-            self.image_failed.append(image_path)
-            pass
+            #   Stain Normalisation
+            try: 
+                image = self.stain_normalizer.process(image)
+                print("WORKED in normalizing")
+            except:
+                print('Warning: {} failed during stain normalization.'.format(image_path))
+                self.image_failed.append(image_path)
+                pass
 
-        #   Build Cell Graph and save it
-        try: 
+            #   Build Cell Graph and save it
+            # try: 
             cg, nuclei_centroid = self.build_cg(image)
             save_graphs(
                 cg_out,
                 g_list = [cg]
             )
-        except:
-            print('Warning: {} failed during cell graph building.'.format(image_path))
-            self.image_failed.append(image_path)
-            pass
+            # except Exception as e:
+            #     print('Warning: {} failed during cell graph building.'.format(image_path))
+            #     print(f"Exception is {e} for cell Graph")
+            #     self.image_failed.append(image_path)
+            #     pass
 
-        #   Build Tissue Graph and save it
-        try:
+            #   Build Tissue Graph and save it
+            # try:
             tissue_graph, tissue_map = self.build_tg(image)
             save_graphs(
                 tg_out,
                 g_list = [tissue_graph]
             )
-        except:
-            print('Warning: {} failed during tissue graph building.'.format(image_path))
-            self.image_failed.append(image_path)
-            pass
+            # except Exception as e:
+            #     print('Warning: {} failed during tissue graph building.'.format(image_path))
+            #     print(f"Exception is {e} for tissue Graph")
+            #     self.image_failed.append(image_path)
+            #     pass
 
-        try: 
-            assignment_matrix = self.assignment_matrix_builder.process(nuclei_centroid, tissue_map)
+            #try: 
+            assignment_matrix = self.assignment_mat_builder.process(nuclei_centroid, tissue_map)
+            #   Create relevant directory if not exist already
+            directory = os.path.dirname(assign_out)
+            os.makedirs(directory, exist_ok=True)
             with h5py.File(assign_out, "w") as output_file:
                 output_file.create_dataset(
                     "assignment_matrix",
@@ -149,20 +166,25 @@ class GraphBuilding:
                     compression="gzip",
                     compression_opts=9,
                 )
-        except:
-            print('Warning: {} failed during assignment matrix generation.'.format(image_path))
-            self.image_failed.append(image_path)
-            pass
+            # except:
+            #     print('Warning: {} failed during assignment matrix generation.'.format(image_path))
+            #     self.image_failed.append(image_path)
+            #     pass
     
-        image_ids_failing = list(set(image_ids_failing))
         print('Out of {} images, {} successful graph generations.'.format(
             len(images_path),
-            len(images_path) - len(image_ids_failing)
+            len(images_path) - len(self.image_failed)
         ))
+        print(self.image_failed)
 
 
 if __name__ == "__main__":
-    image_path = "../Report-nmi-wsi/Images"
-    GB = GraphBuilding()
+    ssl._create_default_https_context = ssl._create_unverified_context # Use it to solve SSL 
+    folder = "../../Report-nmi-wsi"
+    target = "./target_img/target.png"
+    images_path = [file for file in os.listdir(folder) if file.endswith('.png')]
+    GB = GraphBuilding(target)
+    GB.build(folder,"graph","test")
+    # ne = NucleiExtractor()
 
 
