e59cdfede49895b9f80f1118afb5e09b7b5b94a9
PeterGeorgiapg <pacers0406@gmail.com>
2023-08-16 18:04:21 +1000

    datasplitting done

--- main/dataloader.py
+++ main/dataloader.py
@@ -30,34 +30,71 @@ class DiagnosticDataset(Dataset):
     """NMI-WSI dataset dataset."""
     
     def __init__(self,
-            cg_path: str = None,
-            tg_path: str = None,
-            assign_mat_path: str = None,
-            img_path: str = None,
-            cap_path: str = None,
+            split: str = None,
+            base_data_path: str = None,
+            graph_path: str = None,
             load_in_ram: bool = False,):
         # load data
         super(DiagnosticDataset, self).__init__()
-        
-        self.cg_path = cg_path
-        self.tg_path = tg_path
-        self.assign_mat_path = assign_mat_path
-        self.img_path = img_path
-        self.cap_path = cap_path
-        
+        self.graph_path = graph_path
+        self.split = split # Train Test Eval
+        self.cg_path = os.path.join(self.graph_path,"cell_graphs",self.split)
+        self.tg_path = os.path.join(self.graph_path,"tissue_graph",self.split)
+        self.assign_mat_path = os.path.join(self.graph_path,"assignment_mat",self.split)
+        self.img_path = os.path.join(self.base_data_path,"Images", self.split)
+        self.report_file_name = split+"_annoation.json"
+        self.report_path = os.path.join(self.base_data_path,report_file_name)
+
         # Get list of cell graph
-        self.cg = self.get_cell_graph(self.cg_path)
+        self.cg = self.get_cell_graph()
         # Get list of tissue graph
-        self.tg = self.get_tissue_graph(self.tg_path)
+        self.tg = self.get_tissue_graph()
+
+        self.assign_mat = self.get__assign_mat()
         
-        
-        pass
+
+    '''
+    Get the captions and the labels
+    '''
+    def get_captions_labels(list_name, split):
+        image_names = [os.path.splitext(os.path.split(i)[-1])[0]  for i in list_name]
+        image_file_paths = [os.path.join(self.img_path,self.split,i) for i in image_names]
+        with open(self.report_path, 'r') as json_file:
+            report_data = json.load(json_file)
+        captions = [report_data[key]['caption'] for key in new_file_names if key in report_data.keys()]
+        labels = [report_data[key]['label'] for key in new_file_names if key in report_data.keys()]
+        return captions, labels
     
-    def get_cell_graph(cg_path):
-        return None
+    def get_cell_graph(self):
+        self.list_cg_path = glob(os.path.join(self.cg_path, '*.bin'))
+        self.list_cg_path.sort()
+        self.num_cg = len(self.list_cg_path)
+
+        if self.load_in_ram:
+            cell_graphs = [load_graphs(single_cg_path) for single_cg_path in self.list_cg_path]
+            self.cell_graphs = [entry[0][0] for entry in cell_graphs]
     
-    def get_tissue_graph(tg_graph):
-        return None
+    def get_tissue_graph(self):
+        self.list_tg_path = glob(os.path.join(self.tg_path, '*.bin'))
+        self.list_tg_path.sort()
+        self.num_tg = len(self.list_tg_path)
+
+        if self.load_in_ram:
+            tissue_graphs = [load_graphs(single_tg_path) for single_tg_path in self.list_tg_path]
+            self.tissue_graphs = [entry[0][0] for entry in tissue_graphs]
+
+    def get_assign_mat(self):
+        """
+        Load assignment matrices 
+        """
+        self.list_assign_path = glob(os.path.join(self.assign_mat_path, '*.h5'))
+        self.list_assign_path.sort()
+        self.num_assign_mat = len(self.list_assign_path)
+        if self.load_in_ram:
+            self.assign_matrices = [
+                h5_to_tensor(single_assign_path).float().t()
+                    for single_assign_path in self.list_assign_path
+            ]
     
     def __getitem__(self,index):
         # return the cell graph, tissue graph, assignment matrix and the relevant 5 captions
---
+++ main/split_data.py
@@ -0,0 +1,144 @@
+'''
+Only Run this once, when data is extracted
+'''
+import os
+import argparse
+import yaml
+import json
+import shutil
+from glob import glob
+import random
+
+#   Basic Setup
+with open("config/config.yaml", 'r') as yaml_file:
+    config_data = yaml.safe_load(yaml_file)
+
+dataset_path = config_data["dataset_path"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset_path', type = str, 
+        default = "../../Report-nmi-wsi", help = "Path to Dataset")
+args = parser.parse_args()
+if args.dataset_path:
+    dataset_path = args.dataset_path
+
+train_anno_path = os.path.join(dataset_path,"train_annotation.json")
+test_anno_path = os.path.join(dataset_path,"test_annotation.json")
+eval_anno_path = os.path.join(dataset_path,"eval_annotation.json")
+
+full_img_path = os.path.join(dataset_path,"Images")
+
+train_img_folder_path = os.path.join(dataset_path,"Images/train")
+test_img_folder_path = os.path.join(dataset_path,"Images/test")
+eval_img_folder_path = os.path.join(dataset_path,"Images/eval")
+print(train_img_folder_path)
+all_img_name = glob(os.path.join(full_img_path,"*png"))
+# print(len(all_img_name))
+# print(all_img_name[0:2])
+# for i in all_img_name[0:2]:
+#     print(os.path.splitext(os.path.basename(i))[0])
+
+''' 
+Extract image from train_annotations.json into train folder
+'''
+#   Extract all train name
+with open(train_anno_path, 'r') as json_file:
+    train_anno = json.load(json_file)
+
+train_name = list(train_anno.keys())
+
+if os.path.exists(train_img_folder_path):
+    print("exist")
+else:
+    print("not")
+Count = 0
+#   Move them into train folder 
+if not os.path.exists(train_img_folder_path):
+    print("Here")
+    os.makedirs(train_img_folder_path)
+
+    for train_img in train_name:
+        source_path = os.path.join(full_img_path,train_img+".png")
+        destination_path = os.path.join(train_img_folder_path,train_img+".png")
+        if os.path.exists(source_path):
+            shutil.move(source_path,destination_path)
+ 
+all_img_name_after = glob(os.path.join(full_img_path,"*png"))
+print(len(all_img_name_after))
+print(f"{len(all_img_name_after)} images in original /Images folder after processing train image")
+
+
+''' 
+Extract some test_annoations into eval_annotaions, 889 evaluation, 1000 testing
+'''
+print("Split annotaion between evaluation and testing")
+with open(test_anno_path, 'r') as json_file:
+    test_anno = json.load(json_file)
+if len(list(test_anno.keys())) == 1889:
+
+    # Set the seed for reproducibility
+    random.seed(42)
+
+    test_anno_items = list(test_anno.items())
+    random.shuffle(test_anno_items)
+
+    # Split the data into two parts
+    evaluation_data = test_anno_items[:889]
+    testing_data = test_anno_items[889:]
+
+    evaluation_data = dict(evaluation_data)
+    testing_data = dict(testing_data)
+
+
+
+    #  Move evaluation and rewirte test annotaiton
+    with open(eval_anno_path,'w') as write_json_file:
+        json.dump(evaluation_data, write_json_file, indent=4)
+
+
+    with open(test_anno_path, 'w') as write_json_file:
+        json.dump(testing_data, write_json_file, indent = 4)
+
+
+''' 
+Extract image from test_annotaions into train and eval image folders accordingly
+'''
+#   Testing file
+with open(test_anno_path, 'r') as json_file:
+    test_anno = json.load(json_file)
+test_name = list(test_anno.keys())
+assert len(test_name) == 1000
+
+if not os.path.exists(test_img_folder_path):
+    os.makedirs(test_img_folder_path)
+
+    for test_img in test_name:
+        source_path = os.path.join(full_img_path,test_img+".png")
+        destination_path = os.path.join(test_img_folder_path,test_img+".png")
+        if os.path.exists(source_path):
+            shutil.move(source_path,destination_path)
+ 
+all_img_name_after = glob(os.path.join(full_img_path,"*png"))
+print(f"{len(all_img_name_after)} images in original /Images folder after processing test image")
+
+
+
+
+#   Evaluation File
+with open(eval_anno_path, 'r') as json_file:
+    eval_anno = json.load(json_file)
+eval_name = list(eval_anno.keys())
+assert len(eval_name) == 889
+
+if not os.path.exists(eval_img_folder_path):
+    os.makedirs(eval_img_folder_path)
+
+    for eval_img in eval_name:
+        source_path = os.path.join(full_img_path,eval_img+".png")
+        destination_path = os.path.join(eval_img_folder_path,eval_img+".png")
+        if os.path.exists(source_path):
+            shutil.move(source_path,destination_path)
+ 
+all_img_name_after = glob(os.path.join(full_img_path,"*png"))
+print(f"{len(all_img_name_after)} images in original /Images folder after processing test image")
+
